13e38f788d5e59e60a072778d1db6f55d063c1b7
Javi H. Gil <javihgil@softspring.es>
2026-06-24 15:40:51 +0200

    Add target entity resolver coverage

--- tests/DependencyInjection/Compiler/AbstractResolveDoctrineTargetEntityPassTest.php
+++ tests/DependencyInjection/Compiler/AbstractResolveDoctrineTargetEntityPassTest.php
@@ -66,6 +66,60 @@ class AbstractResolveDoctrineTargetEntityPassTest extends TestCase
         self::assertSame([], $definition->getTags());
     }
 
+    public function testItSkipsOptionalMappingsWhenParameterIsEmpty(): void
+    {
+        $container = $this->createContainer();
+        $container->setParameter('app.target_entity.class', '');
+
+        $pass = new class extends AbstractResolveDoctrineTargetEntityPass {
+            protected function getEntityManagerName(ContainerBuilder $container): string
+            {
+                return 'default';
+            }
+
+            public function process(ContainerBuilder $container): void
+            {
+                $this->setTargetEntityFromParameter('app.target_entity.class', TestEntityInterface::class, $container, false);
+            }
+        };
+
+        $pass->process($container);
+
+        $definition = $container->findDefinition('doctrine.orm.listeners.resolve_target_entity');
+
+        self::assertSame([], $definition->getMethodCalls());
+        self::assertSame([], $definition->getTags());
+    }
+
+    public function testItKeepsExistingDoctrineSubscriberTag(): void
+    {
+        $container = $this->createContainer();
+        $container->setParameter('app.target_entity.class', TestEntity::class);
+        $definition = $container->findDefinition('doctrine.orm.listeners.resolve_target_entity');
+        $definition->addTag('doctrine.event_subscriber', ['connection' => 'default']);
+
+        $pass = new class extends AbstractResolveDoctrineTargetEntityPass {
+            protected function getEntityManagerName(ContainerBuilder $container): string
+            {
+                return 'default';
+            }
+
+            public function process(ContainerBuilder $container): void
+            {
+                $this->setTargetEntityFromParameter('app.target_entity.class', TestEntityInterface::class, $container);
+            }
+        };
+
+        $pass->process($container);
+
+        self::assertSame([
+            ['connection' => 'default'],
+        ], $definition->getTag('doctrine.event_subscriber'));
+        self::assertSame([
+            ['addResolveTargetEntity', [TestEntityInterface::class, TestEntity::class, []]],
+        ], $definition->getMethodCalls());
+    }
+
     public function testItFailsWhenRequiredMappingIsMissing(): void
     {
         $container = $this->createContainer();
